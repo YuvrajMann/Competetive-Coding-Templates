7e0d09db0d2edd07e3e34e3496f3eafc532c60a0
Yuvraj <yuvraj1mann@gmail.com>
2021-03-19 18:27:23 +0530

    gcd

--- sievePrimeFactors.cpp
+++ sievePrimeFactors.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define ll long long
@@ -9,7 +9,8 @@ vector<int> primes;
 vector<int> smallest_factor;
 vector<bool> prime;
 
-void sieve(int maximum) {
+void sieve(int maximum)
+{
     maximum = max(maximum, 1);
     smallest_factor.assign(maximum + 1, 0);
     prime.assign(maximum + 1, true);
@@ -17,12 +18,14 @@ void sieve(int maximum) {
     primes = {};
 
     for (int p = 2; p <= maximum; p++)
-        if (prime[p]) {
+        if (prime[p])
+        {
             smallest_factor[p] = p;
             primes.push_back(p);
 
             for (int64_t i = int64_t(p) * p; i <= maximum; i += p)
-                if (prime[i]) {
+                if (prime[i])
+                {
                     prime[i] = false;
                     smallest_factor[i] = p;
                 }
@@ -31,17 +34,22 @@ void sieve(int maximum) {
 
 // Prime factorizes n in worst case O(sqrt n / log n). Requires having run `sieve` up to at least sqrt(n).
 // If we've run `sieve` up to at least n, takes O(log n) time.
-vector<pair<int, int>> prime_factorize(int n) {
+vector<pair<int, int>> prime_factorize(int n)
+{
     int sieve_max = int(smallest_factor.size()) - 1;
-    if (1 <= n && n <= sieve_max * sieve_max) {
+    if (1 <= n && n <= sieve_max * sieve_max)
+    {
         vector<pair<int, int>> result;
 
-        if (n <= sieve_max) {
-            while (n != 1) {
+        if (n <= sieve_max)
+        {
+            while (n != 1)
+            {
                 int p = smallest_factor[n];
                 int exponent = 0;
 
-                do {
+                do
+                {
                     n /= p;
                     exponent++;
                 } while (n % p == 0);
@@ -52,14 +60,17 @@ vector<pair<int, int>> prime_factorize(int n) {
             return result;
         }
 
-        for (auto p : primes) {
+        for (auto p : primes)
+        {
             if (p * p > n)
                 break;
 
-            if (n % p == 0) {
+            if (n % p == 0)
+            {
                 result.emplace_back(p, 0);
 
-                do {
+                do
+                {
                     n /= p;
                     result.back().second++;
                 } while (n % p == 0);
@@ -73,10 +84,12 @@ vector<pair<int, int>> prime_factorize(int n) {
     }
 }
 
-int main() {
+int main()
+{
     sieve(maxN);
     vector<pair<int, int>> p = prime_factorize(33);
-    for ( auto pa : p) {
+    for (auto pa : p)
+    {
         cout << pa.first << " " << pa.second << endl;
     }
     return 0;
