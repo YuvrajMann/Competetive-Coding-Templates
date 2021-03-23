d0503c1f00e29f990be59376151be613fbb42e0f
Yuvraj <yuvraj1mann@gmail.com>
2021-03-23 11:20:55 +0530

    template addition

--- template.cpp
+++ template.cpp
@@ -6,7 +6,15 @@ using namespace std;
 #define pb push_back
 #define mk make_pair
 #define in insert
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define maxHeap priority_queue<int>
+#define minHeap priority_queue<int, vi, greater<int>>
+#define ps(x, y) fixed << setprecision(y) << x
+#define nline "/n"
 
+void solve()
+{
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
